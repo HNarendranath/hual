80135bcde6945cd4b08222cd41b494673f0b58dc
Hari Narendranath <hari.narendranath0510@gmail.com>
2026-07-13 00:00:04 +0100

    feat: temp db writer fillers

---
+++ src/pipeline/db_writer.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::io;
+
+use crate::pipeline::MetadataRecord;
+use crossbeam_channel::Receiver;
+
+fn write_record(job: &MetadataRecord) -> io::Result<()> {
+    todo!()
+}
+
+pub fn run(rx: Receiver<MetadataRecord>) {
+    // for record in rx {
+    //     if let Err(e) = write_record(&record) {
+    //         eprintln!("Error writing {}: {e}", record.dest_path.display());
+    //     }
+    // }
+
+    for record in rx {
+        println!(
+            "{} -> {} | exif: {:?} | thumbnail {} bytes",
+            record.src_path.display(),
+            record.dest_path.display(),
+            record.exif,
+            record.thumbnail.as_ref().map_or(0, |t| t.len()),
+        );
+    }
+}
